10e066fbe211acc41a9840a93631eee492d89307
John Van Enk <vanenkj@gmail.com>
2009-12-31 14:39:14 -0500

    Trashed a bunch of tests.

--- tests/quickcheck.hs
+++ tests/quickcheck.hs
@@ -10,28 +10,17 @@ prop_empty = length (toList empty) == 0
 prop_list :: [Int] -> Bool
 prop_list l = l == (toList . fromList $ l)
 
--- toInfList
--- left
--- right
-
 prop_focus :: [Int] -> Bool
 prop_focus l@(h:_) = (Just h) == (focus . fromList $ l)
 prop_focus e = Nothing == (focus . fromList $ e) -- Empty List
 
-prop_insert :: Int -> [Int] -> Bool
-prop_insert v l = (v:l) == (toList . (flip insert v) . fromList $ l)
+prop_rotL :: [Int] -> Bool
+prop_rotL l@(_:_) = (last l) : (init l) == (toList . rotL . fromList $ l)
+prop_rotL e = e == (toList . rotL . fromList $ e) -- Empty List
 
-prop_remove :: [Int] -> Bool
-prop_remove l@(_:xs) = xs == (toList . remove . fromList $ l)
-prop_remove e = e == (toList . remove . fromList $ e) -- Empty List
-
-prop_prev :: [Int] -> Bool
-prop_prev l@(_:_) = (last l) : (init l) == (toList . prev . fromList $ l)
-prop_prev e = e == (toList . prev . fromList $ e) -- Empty List
-
-prop_next :: [Int] -> Bool
-prop_next l@(h:ts) = ts ++ [h] == (toList . next . fromList $ l)
-prop_next e = e == (toList . next . fromList $ e) -- Empty List
+prop_rotR :: [Int] -> Bool
+prop_rotR l@(h:ts) = ts ++ [h] == (toList . rotR . fromList $ l)
+prop_rotR e = e == (toList . rotR . fromList $ e) -- Empty List
 
 prop_balance :: [Int] -> Bool
 prop_balance l = l == (toList . balance . fromList $ l)
@@ -51,15 +40,32 @@ prop_size l = (length l) == (size . fromList $ l)
 
 main :: IO ()
 main = do
+    putStrLn "prop_empty"
     quickCheck prop_empty
+
+    putStrLn "prop_list"
     quickCheck prop_list
-    quickCheck prop_next
-    quickCheck prop_prev
+    
+    putStrLn "prop_rotR"
+    quickCheck prop_rotR
+
+    putStrLn "prop_rotL"
+    quickCheck prop_rotL
+
+    putStrLn "prop_focus"
     quickCheck prop_focus
-    quickCheck prop_insert
-    quickCheck prop_remove
+
+    putStrLn "prop_balance"
     quickCheck prop_balance
+
+    putStrLn "prop_packL"
     quickCheck prop_packL
+
+    putStrLn "prop_packR"
     quickCheck prop_packR
+
+    putStrLn "prop_isEmpty"
     quickCheck prop_isEmpty
+
+    putStrLn "prop_size"
     quickCheck prop_size
