6e079394977335ec2b85eb2c241b69d293fb1f1b
John Van Enk <john@dorner-john-linux.(none)>
2010-01-04 12:39:40 -0500

    Removed Empty

--- tests/quickcheck.hs
+++ tests/quickcheck.hs
@@ -4,16 +4,12 @@ module Main where
 import Test.QuickCheck
 import Data.CircularList
 
--- Make sure empty really is empty.
-prop_empty :: Bool
-prop_empty = length (toList empty) == 0
-
 -- Make sure converting to/from lists works.
 prop_list :: CList Int -> Bool
 prop_list c = c == (fromList . toList $ c)
 
 prop_focus :: CList Int -> Int -> Bool
-prop_focus c v = (Just v) == (focus $ insertR v c)
+prop_focus c v = v == (focus $ insertR v c)
 
 prop_rot :: CList Int -> Bool
 prop_rot c = c == (rotR $ rotL c)
@@ -24,17 +20,12 @@ prop_packL c = c == (packL c)
 prop_packR :: CList Int -> Bool
 prop_packR c = c == (packR c)
 
-prop_isEmpty :: [Int] -> Bool
-prop_isEmpty l = null l == isEmpty (fromList l)
-
 prop_size :: [Int] -> Bool
+prop_size [] = True
 prop_size l = (length l) == (size . fromList $ l)
 
 main :: IO ()
 main = do
-    putStrLn "prop_empty"
-    quickCheck prop_empty
-
     putStrLn "prop_list"
     quickCheck prop_list
     
@@ -49,9 +40,6 @@ main = do
 
     putStrLn "prop_packR"
     quickCheck prop_packR
-
-    putStrLn "prop_isEmpty"
-    quickCheck prop_isEmpty
 
     putStrLn "prop_size"
     quickCheck prop_size
