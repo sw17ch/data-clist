2aefd62e4168aab41114bbafb1d4b16c11c3ad8a
Xitian9 <morgan.stephen99@gmail.com>
2016-08-19 22:17:10 +1000

    Add minRep benchmark.

--- benchmark/bench.hs
+++ benchmark/bench.hs
@@ -12,7 +12,8 @@ l :: [Integer]
 l = [0..1 * 10^7]
 
 bench_to_from = toList . fromList
-bench_rotR lst = applyNTimes (len * 2) rotR $ fromList lst
+bench_rotR = applyNTimes (len * 2) rotR . fromList
+bench_minRep = minRep . fromList
 
 applyNTimes n f = foldr (.) id $ replicate n f
 
@@ -20,4 +21,5 @@ main :: IO ()
 main = defaultMain
     [ bench "toList . fromList" $ nf bench_to_from l
     , bench "rotR"              $ nf bench_rotR    l
+    , bench "minRep"            $ nf bench_minRep  l
     ]
